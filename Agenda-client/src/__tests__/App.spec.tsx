d68bc6a635d2620d1d4d34d020c0090ed0b55c81
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-25 11:13:33 +0100

    chore: add jest and ci github actions

---
+++ Agenda-client/src/__tests__/App.spec.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+import { render } from "@testing-library/react"
+import App from "../App"
+
+test('demo', () => {
+  expect(true).toBe(true)
+})
+
+test("Renders the main page", () => {
+	render(<App />)
+	expect(true).toBeTruthy()
+})
+
+// test('demo', () => {
+//   expect(true).toBe(false)
+// })
