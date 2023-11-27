dc53b8f7c90e1bd3f226bf5b35afc5dc257fbf2a
Mads Baadsmand <baadsmand@gmail.com>
2023-11-27 13:51:04 +0100

    #26: add simple DailyCurriculum component

--- Agenda-client/src/App.tsx
+++ Agenda-client/src/App.tsx
@@ -1,5 +1,12 @@
+import DailyCurriculum from "./components/dailycurriculum/DailyCurriculum";
+
 function App() {
-  return <h1 className="text-red-500">HELLO AGENDA</h1>;
+  return (
+  <>
+    <h1 className="text-red-500">HELLO AGENDA</h1>
+    <DailyCurriculum></DailyCurriculum>
+  </>
+  );
 }
 
 export default App;
---
+++ Agenda-client/src/components/dailycurriculum/DailyCurriculum.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import curriculum from './testData.json';
+
+export default function DailyCurriculum() {
+  const [day, setDay] = useState(0)
+  const dailyCurriculum = curriculum[day];
+  return (
+    <div className='rounded-md border border-orange-400 divide-dashed w-96 '>
+      <button onClick={()=>setDay(day+1)}>change day</button>
+      <h2>Todays Curriculum</h2>
+      {dailyCurriculum.toy_problem_name && <p>Toy Problem: {dailyCurriculum.toy_problem_name}</p>}
+      <p>Lecture: <a href={dailyCurriculum.lecture_url}>{dailyCurriculum.lecture_name}</a></p>
+      <p>Exercise: {dailyCurriculum.exercise_name}</p>
+    </div>
+  );
+}
