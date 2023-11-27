afd3b1d54e582123f0ecc48da14fb6d8bec9f2ee
Mads Baadsmand <baadsmand@gmail.com>
2023-11-27 17:00:46 +0100

    #26: add router to App component

--- Agenda-client/src/App.tsx
+++ Agenda-client/src/App.tsx
@@ -1,10 +1,17 @@
-import DailyCurriculum from "./components/curriculum/DailyCurriculum";
+import DailyCurriculum from "./components/curriculum/DailyCurriculumDashboardComponent";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import LectureComponent from "./components/curriculum/LectureComponent";
 
 function App() {
   return (
   <>
     <h1 className="text-red-500">HELLO AGENDA</h1>
-    <DailyCurriculum></DailyCurriculum>
+    <BrowserRouter>
+      <Routes>
+       <Route path="/" element={<DailyCurriculum></DailyCurriculum>} />
+       <Route path="/lecture/:lectureId" element={<LectureComponent></LectureComponent>} />
+      </Routes>
+    </BrowserRouter>
   </>
   );
 }
