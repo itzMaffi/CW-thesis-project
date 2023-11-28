e36cf7cfaa70f129b452c74887953b7dfe1db7a0
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-28 14:08:41 +0100

    feat: add profile and progressBar components

--- Agenda-client/src/App.tsx
+++ Agenda-client/src/App.tsx
@@ -1,18 +1,20 @@
 import Dashboard from './components/Dashboard/Dashboard';
 import LectureComponent from './components/curriculum/LectureComponent';
-import { BrowserRouter, Routes, Route}  from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-  <>
-    <h1 className="text-red-500">HELLO AGENDA</h1>
-    <BrowserRouter>
-      <Routes>
-       <Route path="/" element={<Dashboard/>} />
-       <Route path="/lecture/:lectureId" element={<LectureComponent></LectureComponent>} />
-      </Routes>
-    </BrowserRouter>
-  </>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route
+            path="/lecture/:lectureId"
+            element={<LectureComponent></LectureComponent>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
