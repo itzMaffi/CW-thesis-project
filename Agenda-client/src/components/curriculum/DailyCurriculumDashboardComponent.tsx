88bc01d3d02594b69d0f4511254f54e73309403f
Mads Baadsmand <baadsmand@gmail.com>
2023-11-28 10:44:00 +0100

    #29: add pin button to daily curricullum lecture

--- Agenda-client/src/components/curriculum/DailyCurriculumDashboardComponent.tsx
+++ Agenda-client/src/components/curriculum/DailyCurriculumDashboardComponent.tsx
@@ -1,16 +1,36 @@
 import { useState } from 'react';
-import curriculumDb from './data/curriculumDb'
+import curriculumDb from './data/curriculumDb';
 import { Link } from 'react-router-dom';
+import Lecture from './interfaces/Lecture';
 
-export default function DailyCurriculum() {
-  const [day, setDay] = useState(0)
+export default function DailyCurriculum({setPinnedLectures}:{setPinnedLectures:(lectures:Lecture[])=>void}) {
+  const [day, setDay] = useState(0);
   const dailyCurriculum = curriculumDb.getCurriculumOfTheDay(day);
+
+  function onPin() {
+    curriculumDb.pinLectureBy(dailyCurriculum.lecture_id);
+    setPinnedLectures(curriculumDb.getPinnedLectures());
+  }
+
   return (
-    <div className='rounded-md border border-orange-400 divide-dashed w-96 h-48'>
-      <button onClick={()=>setDay(day+1)}>change day</button>
+    <div className="rounded-md border border-orange-400 divide-dashed w-96 h-48">
+      <button onClick={() => setDay(day + 1)}>change day</button>
       <h2>Todays Curriculum</h2>
-      {dailyCurriculum.toy_problem_name && <p>Toy Problem: {dailyCurriculum.toy_problem_name}</p>}
-      <p>Lecture: <Link to={"Lecture/"+dailyCurriculum.lecture_id}>{dailyCurriculum.lecture_name}</Link></p>
+      {dailyCurriculum.toy_problem_name && (
+        <p>Toy Problem: {dailyCurriculum.toy_problem_name}</p>
+      )}
+      <p>
+        Lecture:{' '}
+        <Link to={'Lecture/' + dailyCurriculum.lecture_id}>
+          {dailyCurriculum.lecture_name}
+        </Link>
+      </p>
+      <button
+        onClick={onPin}
+        className="bg-blue-700 rounded-sm text-white p-1.5 self-end w-1/6"
+      >
+        pin lecture
+      </button>
       <p>Exercise: {dailyCurriculum.exercise_name}</p>
     </div>
   );
--- Agenda-client/src/components/curriculum/TempDashboard.tsx
+++ Agenda-client/src/components/curriculum/TempDashboard.tsx
@@ -1,15 +1,16 @@
 import curriculumDb from './data/curriculumDb';
+import {useState} from 'react'
 import DailyCurriculum from './DailyCurriculumDashboardComponent';
 import PinnedLectureDashboardComponent from './PinnedLectureDashboardComponent';
 
 export default function TempDashboard() {
 
-  const pinnedLectures = curriculumDb.getPinnedLectures();
+  const [pinnedLectures, setPinnedLectures] = useState(curriculumDb.getPinnedLectures());
 
   return (
     <>
     <div className='flex align-center justify-center w-screen h-screen'>
-      <DailyCurriculum />
+      <DailyCurriculum setPinnedLectures={setPinnedLectures} />
       {pinnedLectures.map(lecture=> <PinnedLectureDashboardComponent key={lecture.id} lecture={lecture}></PinnedLectureDashboardComponent>)}
     </div>
     
