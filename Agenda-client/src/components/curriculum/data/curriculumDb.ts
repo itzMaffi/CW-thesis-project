3ea378313b60c6a97807a912d768f256d12e477c
Mads Baadsmand <baadsmand@gmail.com>
2023-11-28 12:05:00 +0100

    #29: remove await Promose.resolve

--- Agenda-client/src/components/curriculum/data/curriculumDb.ts
+++ Agenda-client/src/components/curriculum/data/curriculumDb.ts
@@ -15,19 +15,18 @@ export class CurriculumDB {
   }
 
   async getCurriculumOfTheDay(day: number): Promise<Curriculum> {
-    return await Promise.resolve(CurriculumData.curriculum[day]);
+    return CurriculumData.curriculum[day];
   }
 
   async getLectureBy(id: number): Promise<Lecture> {
     const lecture = CurriculumData.lectures.find((el) => el.id == id);
     if (!lecture) throw new Error('lecture does not exist');
 
-    return Promise.resolve(lecture);
+    return lecture;
   }
 
   async pinLecture(lecture: Lecture) {
     if (lecture) this.pinnedLectures.add(lecture);
-    return Promise.resolve();
   }
 
   async pinLectureBy(id: number) {
@@ -36,12 +35,11 @@ export class CurriculumDB {
       const lecture = await this.getLectureBy(id);
       this.pinLecture(lecture);
     } 
-    return Promise.resolve(void 0);
   }
 
   async getPinnedLectures(): Promise<Lecture[]>
   {
-    return Promise.resolve(Array.from(this.pinnedLectures));
+    return Array.from(this.pinnedLectures);
   }
 }
 
