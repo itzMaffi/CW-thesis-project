f4277dbbaf357f4e8978d7f2d15962b981d6eb0a
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-28 21:20:13 +0100

    chore: add prettier

--- Agenda-client/src/components/curriculum/TempDashboard.tsx
+++ Agenda-client/src/components/curriculum/TempDashboard.tsx
@@ -1,28 +1,30 @@
 import curriculumDb from './data/curriculumDb';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import DailyCurriculum from './DailyCurriculumDashboardComponent';
 import PinnedLectureDashboardComponent from './PinnedLectureDashboardComponent';
 import Lecture from './interfaces/Lecture';
 
 export default function TempDashboard() {
-
   const [pinnedLectures, setPinnedLectures] = useState<Lecture[]>([]);
 
-  useEffect(()=> {
-    (async ()=> {
-      const lectures = await curriculumDb.getPinnedLectures()
+  useEffect(() => {
+    (async () => {
+      const lectures = await curriculumDb.getPinnedLectures();
       setPinnedLectures(lectures);
-    }
-    )();
-  },[])
+    })();
+  }, []);
 
   return (
     <>
-    <div className='flex align-center justify-center w-screen h-screen'>
-      <DailyCurriculum setPinnedLectures={setPinnedLectures} />
-      {pinnedLectures.map(lecture=> <PinnedLectureDashboardComponent key={lecture.id} lecture={lecture}></PinnedLectureDashboardComponent>)}
-    </div>
-    
+      <div className="flex align-center justify-center w-screen h-screen">
+        <DailyCurriculum setPinnedLectures={setPinnedLectures} />
+        {pinnedLectures.map((lecture) => (
+          <PinnedLectureDashboardComponent
+            key={lecture.id}
+            lecture={lecture}
+          ></PinnedLectureDashboardComponent>
+        ))}
+      </div>
     </>
   );
 }
