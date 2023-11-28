f04dda9af30819a2c5d2e28b1a9a8edc95aca465
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-28 20:48:18 +0100

    chore: add custom colors to tailwind

--- Agenda-client/tailwind.config.js
+++ Agenda-client/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cw-orange': '#FF7B10',
+        'cw-light-orange': '#FFE6D8',
+        'cw-dark-orange': '#984400',
+      },
+    },
   },
   plugins: [],
 };
