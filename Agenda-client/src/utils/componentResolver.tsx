e3bf91971dd962b9e392a2163ded8e34c01d5ced
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-28 21:06:29 +0100

    refactor: adjust style and colors

--- Agenda-client/src/utils/componentResolver.tsx
+++ Agenda-client/src/utils/componentResolver.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import {
   DummyHelpRequest,
-  DummyLogin,
   DummyLectureOfTheDay,
 } from '../components/Dummies/Dummies';
 import { ComponentsMapper } from './types';
+import { UserProfile } from '../components/userProfile/UserProfile';
 
 export default function resolveComponent(componentType: string) {
   const components: ComponentsMapper = {
-    login: <DummyLogin />,
+    login: <UserProfile />,
     helpRequest: <DummyHelpRequest />,
     lectureOfTheDay: <DummyLectureOfTheDay />,
   };
