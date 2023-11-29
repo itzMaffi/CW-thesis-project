aee046305e09cadf52473b2a02ccc8017efb3f7d
Michele Maffei <michele.muffin@gmail.com>
2023-11-29 19:49:54 +0100

    Add extra style

--- Agenda-client/src/components/curriculum/Announcement.tsx
+++ Agenda-client/src/components/curriculum/Announcement.tsx
@@ -33,11 +33,11 @@ const Announcement: React.FC = () => {
   }, []);
 
   return (
-    <div className="p-8">
-      <h1 className="text-3xl mb-4">Announcements</h1>
-      <ul>
+    <div className="relative">
+      <h1 className="text-lg text-white bg-cw-orange font-bold text-center w-full sticky top-0 p-2">Announcements</h1>
+      <ul className='px-2 py-4'>
         {slackMessages.map((message: string, index: number) => (
-          <li key={index} className="text-lg mb-2">
+          <li key={index} className="text-lg mb-2 bg-cw-light-orange rounded-lg px-4 py-2 text-gray-700">
             {message}
           </li>
         ))}
