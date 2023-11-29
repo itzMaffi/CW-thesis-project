395a680c3da028e1ab223275f33de10cfb4dcac4
Guangzheng Li <guangzheng.li2018@gmail.com>
2023-11-29 12:19:09 +0100

    working progress on help request

--- Agenda-client/src/utils/componentResolver.tsx
+++ Agenda-client/src/utils/componentResolver.tsx
@@ -1,4 +1,5 @@
 import {
+<<<<<<< HEAD
   DummyHelpRequest,
   DummyLectureOfTheDay,
 } from '../components/Dummies/Dummies';
@@ -9,6 +10,18 @@ export default function resolveComponent(componentType: string) {
   const components: ComponentsMapper = {
     login: <UserProfile />,
     helpRequest: <DummyHelpRequest />,
+=======
+  DummyLogin,
+  DummyLectureOfTheDay,
+} from '../components/Dummies/Dummies';
+import { ComponentsMapper } from './types';
+import NewHelpRequest from '../components/NewHelpRequest/NewHelpRequest';
+
+export default function resolveComponent(componentType: string) {
+  const components: ComponentsMapper = {
+    login: <DummyLogin />,
+    helpRequest: <NewHelpRequest />,
+>>>>>>> ac3df35 (working progress on help request)
     lectureOfTheDay: <DummyLectureOfTheDay />,
   };
 
