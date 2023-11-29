3637c7044779ce01e24bd969e635f1952ddf7a16
Guangzheng Li <guangzheng.li2018@gmail.com>
2023-11-29 14:10:26 +0100

    working on styling for the component

--- Agenda-client/src/utils/componentResolver.tsx
+++ Agenda-client/src/utils/componentResolver.tsx
@@ -1,27 +1,12 @@
-import {
-<<<<<<< HEAD
-  DummyHelpRequest,
-  DummyLectureOfTheDay,
-} from '../components/Dummies/Dummies';
+import { DummyLectureOfTheDay } from '../components/Dummies/Dummies';
 import { ComponentsMapper } from './types';
 import { UserProfile } from '../components/userProfile/UserProfile';
-
-export default function resolveComponent(componentType: string) {
-  const components: ComponentsMapper = {
-    login: <UserProfile />,
-    helpRequest: <DummyHelpRequest />,
-=======
-  DummyLogin,
-  DummyLectureOfTheDay,
-} from '../components/Dummies/Dummies';
-import { ComponentsMapper } from './types';
 import NewHelpRequest from '../components/NewHelpRequest/NewHelpRequest';
 
 export default function resolveComponent(componentType: string) {
   const components: ComponentsMapper = {
-    login: <DummyLogin />,
+    login: <UserProfile />,
     helpRequest: <NewHelpRequest />,
->>>>>>> ac3df35 (working progress on help request)
     lectureOfTheDay: <DummyLectureOfTheDay />,
   };
 
