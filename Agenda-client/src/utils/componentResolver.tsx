4c937c0974b411c65d0d7130565b0d9a7b9ca556
Michele Maffei <michele.muffin@gmail.com>
2023-11-29 19:26:20 +0100

    Add announcement widget w/ widget types

--- Agenda-client/src/utils/componentResolver.tsx
+++ Agenda-client/src/utils/componentResolver.tsx
@@ -4,15 +4,20 @@ import { WidgetType } from './Widget';
 import { ComponentsMapper } from './types';
 import { UserProfile } from '../components/userProfile/UserProfile';
 import NewHelpRequest from '../components/NewHelpRequest/NewHelpRequest';
+import Announcement from '../components/curriculum/Announcement';
 
-
-export default function resolveComponent(componentType: WidgetType, layoutKey:string) {
+export default function resolveComponent(
+  componentType: WidgetType,
+  layoutKey: string
+) {
   const components: ComponentsMapper = {
     [WidgetType.login]: <UserProfile />,
     [WidgetType.helpRequest]: <NewHelpRequest />,
     [WidgetType.lectureOfTheDay]: <DailyCurriculum />,
-    [WidgetType.pinnedLecture]:<PinnedLectureDashboardComponent layoutKey={layoutKey} />
+    [WidgetType.pinnedLecture]: (
+      <PinnedLectureDashboardComponent layoutKey={layoutKey} />
+    ),
+    [WidgetType.announcement]: <Announcement />,
   };
-
   return components[componentType] || null;
 }
