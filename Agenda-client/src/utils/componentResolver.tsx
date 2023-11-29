3055f50f82dff4a690df32fd4ae9a66db89b9023
Guangzheng Li <guangzheng.li2018@gmail.com>
2023-11-29 18:42:05 +0100

    fix the merge conflicts

--- Agenda-client/src/utils/componentResolver.tsx
+++ Agenda-client/src/utils/componentResolver.tsx
@@ -5,17 +5,21 @@ import { ComponentsMapper } from './types';
 import { UserProfile } from '../components/userProfile/UserProfile';
 import NewHelpRequest from '../components/NewHelpRequest/NewHelpRequest';
 import Announcement from '../components/curriculum/Announcement';
-
 export default function resolveComponent(
+  
   componentType: WidgetType,
-  layoutKey: string
+ 
+  layoutKey:  string
+
 ) {
   const components: ComponentsMapper = {
     [WidgetType.login]: <UserProfile />,
     [WidgetType.helpRequest]: <NewHelpRequest />,
     [WidgetType.lectureOfTheDay]: <DailyCurriculum />,
     [WidgetType.pinnedLecture]: (
+       (
       <PinnedLectureDashboardComponent layoutKey={layoutKey} />
+    ),
     ),
     [WidgetType.announcement]: <Announcement />,
   };
