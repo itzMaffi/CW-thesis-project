3943946ec08028733e9f463fe860f3c647b42e61
Mads Baadsmand <baadsmand@gmail.com>
2023-11-30 17:16:17 +0100

    style: add styling to LectureOfTheDay

--- Agenda-client/src/components/curriculum/DailyCurriculumDashboardComponent.tsx
+++ Agenda-client/src/components/curriculum/DailyCurriculumDashboardComponent.tsx
@@ -3,6 +3,8 @@ import curriculumDb from './data/curriculumDb';
 import { Link } from 'react-router-dom';
 import Curriculum from './interfaces/Curriculum';
 import pinLecture from './LectureService';
+import { RiPushpinLine } from 'react-icons/ri';
+import logo from '../../assets/advancedjs.png';
 
 export default function DailyCurriculum() {
   const [day, setDay] = useState(0);
@@ -22,25 +24,40 @@ export default function DailyCurriculum() {
 
   return (
     dailyCurriculum && (
-      <div className="w-full h-full">
-        <button onClick={() => setDay(day + 1)}>change day</button>
-        <h2>Todays Curriculum</h2>
-        {dailyCurriculum.toy_problem_name && (
-          <p>Toy Problem: {dailyCurriculum.toy_problem_name}</p>
-        )}
-        <p>
-          Lecture:{' '}
-          <Link to={'Lecture/' + dailyCurriculum.lecture_id}>
-            {dailyCurriculum.lecture_name}
-          </Link>
-        </p>
-        <button
-          onClick={onPin}
-          className="bg-blue-700 rounded-sm text-white p-1.5 self-end w-1/6"
-        >
-          pin lecture
-        </button>
-        <p>Exercise: {dailyCurriculum.exercise_name}</p>
+      <div className="w-full h-full flex flex-col">
+        <h2 className="bg-cw-orange min-w-full text-center p-2 text-lg font-bold font-sans text-white rounded-tr-md rounded-tl-md">
+          Lecture of the day
+        </h2>
+        <div className="pl-6 pr-6 pt-2 pb-4 flex flex-1 w-full">
+          <div className="grow w-1/2">
+            {dailyCurriculum.toy_problem_name && (
+              <div>
+                <h2 className="text-gray-400">Toy Problem:</h2>
+                <p>{dailyCurriculum.toy_problem_name}</p>
+              </div>
+            )}
+            <h2 className="text-gray-400">Lecture:</h2>
+            <p className="flex">
+              <Link to={'Lecture/' + dailyCurriculum.lecture_id}>
+                {dailyCurriculum.lecture_name}
+              </Link>
+              <RiPushpinLine
+                onClick={onPin}
+                className="ml-4 cursor-pointer text-cw-orange"
+              />
+            </p>
+            <h2 className="text-gray-400">Exercise</h2>
+            <p>{dailyCurriculum.exercise_name}</p>
+            <button onClick={() => setDay(day + 1)} className="text-xs">
+              DEBUG: change day
+            </button>
+          </div>
+          <div className="grow w-1/2 flex justify-center items-center" >
+            <div className='h-40 w-40'>
+              <img className="rounded-lg object-contain " src={logo}></img>
+            </div>
+          </div>
+        </div>
       </div>
     )
   );
