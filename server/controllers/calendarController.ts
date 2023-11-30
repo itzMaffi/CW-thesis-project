3e1c85a1d8a8886700dd60a707821041777ef544
Michele Maffei <michele.muffin@gmail.com>
2023-11-30 19:52:42 +0100

    Add barebone controller (no error handling)

--- server/controllers/calendarController.ts
+++ server/controllers/calendarController.ts
@@ -1,5 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { promises as fs } from 'fs';
 
-export async function getCalendarEvents(req: Request, res: Response){
- res.send('OK')
+export async function getCalendarEvents(req: Request, res: Response) {
+  const eventsRaw = await fs.readFile('./events.json', 'utf-8');
+  res.send(JSON.parse(eventsRaw));
 }
