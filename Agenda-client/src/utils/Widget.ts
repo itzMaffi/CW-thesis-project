92af77de9567aeed7af12b6581394c7580f2f3ef
Michele Maffei <michele.muffin@gmail.com>
2023-11-30 10:04:54 +0100

    Add calendar widget to type and resolver with default size and position in layout

--- Agenda-client/src/utils/Widget.ts
+++ Agenda-client/src/utils/Widget.ts
@@ -15,4 +15,5 @@ export enum WidgetType {
   lectureOfTheDay,
   pinnedLecture,
   announcement,
+  calendar,
 }
--- Agenda-client/src/utils/componentResolver.tsx
+++ Agenda-client/src/utils/componentResolver.tsx
@@ -5,6 +5,7 @@ import { ComponentsMapper } from './types';
 import { UserProfile } from '../components/userProfile/UserProfile';
 import NewHelpRequest from '../components/NewHelpRequest/NewHelpRequest';
 import Announcement from '../components/curriculum/Announcement';
+import Calendar from '../components/Calendar/Calendar';
 export default function resolveComponent(
   componentType: WidgetType,
 
@@ -18,6 +19,7 @@ export default function resolveComponent(
       <PinnedLectureDashboardComponent layoutKey={layoutKey} />
     ),
     [WidgetType.announcement]: <Announcement />,
+    [WidgetType.calendar]: <Calendar />,
   };
   return components[componentType] || null;
 }
