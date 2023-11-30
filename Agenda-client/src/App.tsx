1d1441b1d1ea4d041a28add0299b5707a9bd8568
Dinara Bekeshova <dbekeshova@gmail.com>
2023-11-30 15:21:59 +0100

    feat: setup oauth with google

--- Agenda-client/src/App.tsx
+++ Agenda-client/src/App.tsx
@@ -3,10 +3,33 @@ import LectureComponent from './components/curriculum/LectureComponent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SyllabusPage from './components/curriculum/SyllabusPage';
 import { Navbar } from './components/Navbar/Navbar';
+import googleButton from './assets/google_signin_assets/web//svg/dark/web_dark_rd_SI.svg';
+
+// OAuth
+function navigate(url: string) {
+  window.location.href = url;
+}
+
+// TODO change this to BE url
+async function auth() {
+  const response = await fetch('http://127.0.0.1:3000/request', {
+    method: 'post',
+  });
+
+  const data = await response.json();
+  console.log('data from App.tsx:', data);
+  navigate(data.url);
+}
 
 function App() {
   return (
     <>
+      {/* TODO implement the proper frontend */}
+      <h3>Google OAuth</h3>
+      <button className="btn-auth" type="button" onClick={() => auth()}>
+        <img className="btn-auth-img" src={googleButton} alt="google sign in" />
+      </button>
+
       <BrowserRouter>
         <Navbar />
         <Routes>
--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,34 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-import router from './router';
+import router from './routers/router';
+import authRouter from './routers/oauth';
+import requestRouter from './routers/request';
 
 const app: Express = express();
-const port = 3000;
+const PORT = 3000;
+const HOST = 'http://127.0.0.1';
+
+app.options('*', function (req, res, next) {
+  // TODO change this to the correct URL of frontend for prod
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', [
+    'X-Requested-With',
+    'content-type',
+    'credentials',
+  ]);
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.status(200);
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/oauth', authRouter);
+app.use('/request', requestRouter);
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Proxy server is running on port ${port} ✅`);
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${HOST}:${PORT} ✅`);
 });
