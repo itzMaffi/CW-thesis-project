57e01a1a19511d50a865806ef11a96d351e78574
Mads Baadsmand <baadsmand@gmail.com>
2023-11-30 19:22:43 +0100

    feat unpin: add unpin to pinnedlecture component

--- Agenda-client/src/components/curriculum/PinnedLectureDashboardComponent.tsx
+++ Agenda-client/src/components/curriculum/PinnedLectureDashboardComponent.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Lecture from './interfaces/Lecture';
 import curriculumDB from './data/curriculumDb';
+import LecturePin from './LecturePin';
 
 export default function PinnedLectureDashboardComponent({
   layoutKey,
@@ -20,10 +21,21 @@ export default function PinnedLectureDashboardComponent({
   return (
     lecture && (
       <div className="h-full w-full p-2">
-          <h3 className=' text-cw-orange font-bold text-xl mb-2'>{lecture.name}</h3>
-        <p className='text-gray-700'>{lecture.summary}</p>
-        <Link className="absolute bottom-0 right-0" to={'Lecture/' + lecture.id}>
-          <p className='bg-cw-orange px-2 py-1 rounded-tl-xl text-white font-semibold'>Go to lecture</p>
+        <div className="float-right text-cw-orange font-bold text-xl mb-2">
+          <LecturePin lectureId={lecture.id}></LecturePin>
+        </div>
+        <h3 className=" text-cw-orange font-bold text-xl mb-2">
+          {lecture.name}
+        </h3>
+
+        <p className="text-gray-700">{lecture.summary}</p>
+        <Link
+          className="absolute bottom-0 right-0"
+          to={'Lecture/' + lecture.id}
+        >
+          <p className="bg-cw-orange px-2 py-1 rounded-tl-xl text-white font-semibold">
+            Go to lecture
+          </p>
         </Link>
       </div>
     )
