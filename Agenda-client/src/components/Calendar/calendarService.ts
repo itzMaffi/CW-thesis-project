4a58f059cc5a41078a4d349757003a2303738e9d
Michele Maffei <michele.muffin@gmail.com>
2023-12-01 10:57:00 +0100

    Add event sorting

--- Agenda-client/src/components/Calendar/calendarService.ts
+++ Agenda-client/src/components/Calendar/calendarService.ts
@@ -1,6 +1,16 @@
+export type Event = {
+  summary: string;
+  start: string;
+  end: string;
+};
+
+function sortEvents(aEvent: Event, bEvent: Event) {
+  return Date.parse(aEvent.start) - Date.parse(bEvent.start);
+}
+
 export async function getTodayEvents() {
   const data = await fetch('http://localhost:3000/calendar');
   const eventList = await data.json();
-
+  eventList.sort(sortEvents);
   return eventList;
 }
