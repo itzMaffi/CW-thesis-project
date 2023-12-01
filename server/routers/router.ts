b001d4e8d71efc6026bf52dcbb0ade8ef3845bbb
Michele Maffei <michele.muffin@gmail.com>
2023-12-01 16:24:10 +0100

    Fix merge

--- server/routers/router.ts
+++ server/routers/router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { getSlackMessages } from '../controllers/slackController';
+import { getCalendarEvents } from '../controllers/calendarController';
 const router = Router();
 
 router.get('/slack-messages', getSlackMessages);
+router.get('/calendar', getCalendarEvents);
 
 export default router;
