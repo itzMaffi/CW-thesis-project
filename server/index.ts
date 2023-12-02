7c9c34c74ac8e9d4f19aefac407529c551e3264c
Oguz Tugan <t-oguz@tugx.de>
2023-12-02 15:10:37 +0100

    if dev or production change url accordingly

--- server/index.ts
+++ server/index.ts
@@ -5,16 +5,19 @@ import authRouter from './routers/oauth';
 import requestRouter from './routers/request';
 import { getTodayEventsJob } from './jobs';
 
-
 const app: Express = express();
-const PORT = 3000;
-const HOST = 'localhost';
+const PORT = process.env.PORT || 3000;
 
-getTodayEventsJob.start()
+const BACKEND_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.URL
+    : 'http://localhost:3000';
+
+getTodayEventsJob.start();
 
 app.options('*', function (req, res, next) {
   // TODO change this to the correct URL of frontend for prod
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', BACKEND_URL);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', [
     'X-Requested-With',
@@ -26,8 +29,6 @@ app.options('*', function (req, res, next) {
   next();
 });
 
-
-
 app.use(cors());
 app.use(express.json());
 
@@ -36,5 +37,5 @@ app.use('/request', requestRouter);
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${HOST}:${PORT} ✅`);
+  console.log(`Proxy server is running on ${BACKEND_URL} ✅`);
 });
