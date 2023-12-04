1c671d54e3c290934fbe3cbfc52db3b3446e4b60
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-04 17:10:19 +0100

    fix: jest mocking import meta

--- Agenda-client/jest.config.ts
+++ Agenda-client/jest.config.ts
@@ -1,9 +1,32 @@
 export default {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.(tsx|ts)?$': 'ts-jest',
-    // process `*.tsx` files with `ts-jest`
+    '^.+\\.(tsx|ts)?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+              options: {
+                metaObjectReplacement: {
+                  url: 'https://www.url.com',
+                  env: {
+                    PROD: false,
+                    DEV: true,
+                  },
+                  status: 2,
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.ts',
