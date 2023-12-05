1a8051c18ed277b9a255d2d412e10aa9e3c025e0
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-05 20:47:58 +0100

    chore: remove todo comments

--- Agenda-client/src/components/LoginPage/LogIn.tsx
+++ Agenda-client/src/components/LoginPage/LogIn.tsx
@@ -22,12 +22,10 @@ export const LogIn: FC<{
     })();
   }, [navigate, setIsAuthenticated]);
 
-  // TODO: OAuth - remove
   function navi(url: string) {
     window.location.href = url;
   }
 
-  // TODO change this to BE url
   async function auth() {
     const response = await fetch(`${BACKEND_URL}/request`, {
       method: 'post',
