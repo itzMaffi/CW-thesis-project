a4a2f0757ef22dadc947cbf4d1c431e72f8b5ebb
Oguz Tugan <t-oguz@tugx.de>
2023-12-05 16:49:18 +0100

    fix variable quiz

--- server/routers/router.ts
+++ server/routers/router.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import { getSlackMessages } from '../controllers/slackController';
 import { getCalendarEvents } from '../controllers/calendarController';
 import { dbread } from '../controllers/dbController';
-
+import { getQuizQuestions } from '../controllers/quizController';
+import connectDB from '../models/db';
 const router = Router();
+
+connectDB();
 
 router.get('/slack-messages', getSlackMessages);
 router.get('/calendar', getCalendarEvents);
 router.get('/dbread', dbread); // test the db, can be removed later on, gets entries from db
+router.get('/quiz-questions/:lectureName', getQuizQuestions);
 
 export default router;
