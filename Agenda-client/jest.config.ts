d96fa1c7716aa73852fec688bf6dc2ceb3c34d74
Michele Maffei <michele.muffin@gmail.com>
2023-12-05 20:13:18 +0100

    Add/edit config for client testing

--- Agenda-client/jest.config.ts
+++ Agenda-client/jest.config.ts
@@ -6,6 +6,7 @@ export default {
     '^.+\\.(tsx|ts)?$': [
       'ts-jest',
       {
+        tsconfig: 'tsconfig.spec.json',
         diagnostics: {
           ignoreCodes: [1343],
         },
@@ -33,4 +34,5 @@ export default {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.ts',
     '\\.(css)$': '<rootDir>/src/__mocks__/styleMock.ts',
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
