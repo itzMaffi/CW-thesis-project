e86d708da38e4ce36d66f054d2d8050d7787d004
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-05 23:41:15 +0100

    chore: fix jest configs

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,8 +1,14 @@
-export default {
+import type { Config } from '@jest/types';
+import path from 'path';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globalSetup: '<rootDir>/jest.global-setup.ts',
+  rootDir: path.join(__dirname, './'), // Set root directory
+  globalSetup: './jest.global-setup.ts',
   verbose: true,
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
 };
+
+export default config;
