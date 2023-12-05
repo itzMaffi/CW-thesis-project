93962f669bbe35ed40d1cb7159ee71c412764af5
Michele Maffei <michele.muffin@gmail.com>
2023-12-05 20:14:08 +0100

    Add calendar compoenent tests

--- Agenda-client/src/__tests__/Calendar.spec.tsx
+++ Agenda-client/src/__tests__/Calendar.spec.tsx
@@ -1,1 +1,62 @@
-import Calendar from "../components/Calendar/Calendar";
+import { render, screen } from '@testing-library/react';
+import { getTodayEvents } from '../components/Calendar/calendarService';
+import Calendar from '../components/Calendar/Calendar';
+
+jest.mock('../components/Calendar/calendarService');
+
+const mockResponse = {
+  resolve: [
+    {
+      id: 1,
+      start: '2023-12-05T09:00:00Z',
+      end: '2023-12-05T10:00:00Z',
+      summary: 'Introduction to JavaScript ES6 features',
+    },
+    {
+      id: 2,
+      start: '2023-12-05T11:00:00Z',
+      end: '2023-12-05T12:00:00Z',
+      summary:
+        'Understanding Asynchronous JavaScript: Callbacks, Promises, and Async/Await',
+    },
+    {
+      id: 3,
+      start: '2023-12-05T13:00:00Z',
+      end: '2023-12-05T14:00:00Z',
+      summary: 'Exploring JavaScript Frameworks: React vs Vue vs Angular',
+    },
+  ],
+  reject: 'error',
+};
+
+describe('Calendar component', () => {
+  it('should render today events if call resolves', async () => {
+    (getTodayEvents as jest.Mock).mockResolvedValueOnce(mockResponse.resolve);
+
+    const { findByText } = render(<Calendar widget={{ id: '1', type: 6 }} />);
+
+    expect(
+      await findByText(mockResponse.resolve[0].summary)
+    ).toBeInTheDocument();
+    expect(
+      await findByText(mockResponse.resolve[1].summary)
+    ).toBeInTheDocument();
+    expect(
+      await findByText(mockResponse.resolve[2].summary)
+    ).toBeInTheDocument();
+
+    expect(
+      screen.queryByText(`There was an error getting today's events`)
+    ).not.toBeInTheDocument();
+  });
+
+  it('should render error if call rejects', async () => {
+    (getTodayEvents as jest.Mock).mockRejectedValue(mockResponse.reject);
+
+    const { findByText } = render(<Calendar widget={{ id: '1', type: 6 }} />);
+
+    expect(
+      await findByText(`There was an error getting today's events`)
+    ).toBeInTheDocument();
+  });
+});
