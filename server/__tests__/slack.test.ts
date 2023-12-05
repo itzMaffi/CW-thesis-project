c6049150f986adf08486d4adf6274038ed76e3af
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-05 15:26:59 +0100

    test: mock mongodb with mongoose

--- server/__tests__/slack.test.ts
+++ server/__tests__/slack.test.ts
@@ -1,9 +1,15 @@
 import request from 'supertest';
 import { createServer } from '../server';
+import messageModel from '../models/messageModel';
+const mockingoose = require('mockingoose');
 
 const app = createServer();
 
 describe('Slack API', () => {
+  beforeAll(async () => {
+    // mock mongoose connection
+    mockingoose(messageModel).toReturn({}, 'find');
+  });
   it('should return 200 OK', async () => {
     const res = await request(app).get('/slack-messages');
     expect(res.status).toBe(200);
