1a040475597bd73e45f035bc499a86ba00e7125e
Mads Baadsmand <baadsmand@gmail.com>
2023-12-05 16:31:27 +0100

    Refactor to use classes for each widget

--- Agenda-client/src/components/widget/Widget.ts
+++ Agenda-client/src/components/widget/Widget.ts
@@ -1,4 +1,6 @@
-export class Widget {
+import { ReactNode } from "react";
+
+export abstract class Widget {
   x:number = 0;
   y:number = 0;
   width:number = 1;
@@ -8,34 +10,27 @@ export class Widget {
   type: WidgetType;
   dataId?: string;
 
+  protected _component:ReactNode;
+
   constructor(type: WidgetType, dataId?: string) {
     const layoutId: string = crypto.randomUUID();
     this.id = layoutId;
     this.type = type;
     this.dataId = dataId;
+  }
 
-    switch(type){
-      case WidgetType.pinnedLecture:
-      case WidgetType.curriculumProgress:
-      case WidgetType.userProfile:
-        this.height = 1;
-        this.width = 1;
-        break;
-      case WidgetType.lectureOfTheDay:
-      case WidgetType.quiz:
-      case WidgetType.stackOverflow:
-        this.height = 2;
-        this.width = 2;
-        break;
-      case WidgetType.announcement:
-      case WidgetType.helpRequest:
-        this.height = 2;
-        this.width = 1;
-        break;
-      case WidgetType.calendar:
-        this.height = 4;
-        this.width = 1;
-        break;
+  get component() {
+    return this._component;
+  }
+
+  get layout() {
+    return {
+      i: this.id,
+      x: this.x,
+      y: this.y,
+      h: this.height,
+      w: this.width,
+      isResizable: false,
     }
   }
 }
