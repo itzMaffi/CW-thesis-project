482534b3975c03f3646fa59f282a2687893edc1c
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-05 11:39:33 +0100

    chore: separate createServer into its own file

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,13 @@
-import express, { Express } from 'express';
-import cors from 'cors';
-import router from './routers/router';
-import authRouter from './routers/oauth';
-import requestRouter from './routers/request';
+import { Express } from 'express';
 import { getTodayEventsJob } from './jobs';
+import { createServer } from './server';
 
-const app: Express = express();
+const app: Express = createServer();
 
 const PORT = process.env.PORT ?? 3000;
 const HOST = 'http://localhost';
-const FRONTEND_URL = process.env.FRONTEND_URL ?? 'http://localhost:5173';
 
 getTodayEventsJob.start();
-
-app.options('*', function (req, res, next) {
-  // TODO change this to the correct URL of frontend for prod
-  res.header('Access-Control-Allow-Origin', `${FRONTEND_URL}`);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', [
-    'X-Requested-With',
-    'content-type',
-    'credentials',
-  ]);
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.status(200);
-  next();
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/oauth', authRouter);
-app.use('/request', requestRouter);
-app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on ${HOST} ✅`);
