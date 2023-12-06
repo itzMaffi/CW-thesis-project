ee48dc3eadf52c6f09781f117bfbfbb3a0531d41
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-06 16:16:43 +0100

    test(server): mock slack api using nock

--- server/__tests__/slack.test.ts
+++ server/__tests__/slack.test.ts
@@ -1,11 +1,19 @@
 import request from 'supertest';
 import { createServer } from '../server';
 import { setCachedMessages } from '../controllers/slackController';
+import nock from 'nock';
 
 const app = createServer();
 
 describe('GET /slack-messages', () => {
   beforeEach(() => {
+    nock('https://slack.com')
+      .get('/api/conversations.history')
+      .query({ channel: 'C067J7TJGRK', limit: 2 })
+      .reply(200, {
+        ok: true,
+        messages: [{ text: 'Mocked message 1' }, { text: 'Mocked message 2' }],
+      });
     setCachedMessages(['Mocked message 1', 'Mocked message 2']);
   });
 
