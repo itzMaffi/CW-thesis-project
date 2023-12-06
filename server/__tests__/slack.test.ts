0eb698016e94c6de5274d16e7dab2f799656b4fa
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-06 16:00:18 +0100

    test(server): add slack and db unit tests

--- server/__tests__/slack.test.ts
+++ server/__tests__/slack.test.ts
@@ -1,12 +1,22 @@
 import request from 'supertest';
 import { createServer } from '../server';
-// import { describe, it, expect } from '@jest/globals';
+import { setCachedMessages } from '../controllers/slackController';
 
 const app = createServer();
 
-describe('Slack API', () => {
-  it('should return 200 OK', async () => {
-    const res = await request(app).get('/slack-messages');
-    expect(res.status).toBe(200);
+describe('GET /slack-messages', () => {
+  beforeEach(() => {
+    setCachedMessages(['Mocked message 1', 'Mocked message 2']);
+  });
+
+  afterEach(() => {
+    // reset cachedMessages after each test
+    setCachedMessages([]);
+  });
+
+  it('should return slack messages', async () => {
+    const response = await request(app).get('/slack-messages');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(['Mocked message 1', 'Mocked message 2']);
   });
 });
