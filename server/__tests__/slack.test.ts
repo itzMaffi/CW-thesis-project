623933f34577e6a404f3aa35ed0799fd924f0fcf
Dinara Bekeshova <dbekeshova@gmail.com>
2023-12-06 20:39:05 +0100

    refactor: slackController and add unit tests

--- server/__tests__/slack.test.ts
+++ server/__tests__/slack.test.ts
@@ -1,32 +1,27 @@
+// Global module mock (outside of any function, at the top of the file)
+jest.mock('../controllers/slackController', () => ({
+  initializeMessageFetching: jest.fn(),
+  getSlackMessages: jest.fn(),
+}));
+
 import request from 'supertest';
 import { createServer } from '../server';
-import { setCachedMessages } from '../controllers/slackController';
-import nock from 'nock';
+import { getSlackMessages } from '../controllers/slackController';
 
 const app = createServer();
 
 describe('GET /slack-messages', () => {
   beforeEach(() => {
-    const token = 'mocked_slack_token'; // Mock token for testing
-
-    nock('https://slack.com')
-      .get('/api/conversations.history')
-      .query({ channel: 'C067J7TJGRK', limit: 2 })
-      .matchHeader('Authorization', `Bearer ${token}`)
-      .reply(200, {
-        ok: true,
-        messages: [{ text: 'Mocked message 1' }, { text: 'Mocked message 2' }],
-      });
-    setCachedMessages(['Mocked message 1', 'Mocked message 2']);
+    (getSlackMessages as jest.Mock).mockImplementation((req, res) => {
+      res.status(200).json(['Mocked message 1', 'Mocked message 2']);
+    });
   });
 
   afterEach(() => {
-    // reset cachedMessages after each test
-    setCachedMessages([]);
-    nock.cleanAll();
+    jest.clearAllMocks();
   });
 
-  it('should return slack messages', async () => {
+  it('should return mocked slack messages', async () => {
     const response = await request(app).get('/slack-messages');
     expect(response.status).toBe(200);
     expect(response.body).toEqual(['Mocked message 1', 'Mocked message 2']);
---
+++ server/utils/slack.ts
@@ -0,0 +1,36 @@
+interface ISlackApiResponse {
+  ok: boolean;
+  messages: {
+    text: string;
+  }[];
+}
+
+const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+
+export async function fetchSlackMessages() {
+  const channelId = SLACK_CHANNEL;
+  const token = SLACK_TOKEN;
+  const limit = 2; // limits to the last 2 slack messages aka only show last 2 messages in dashboard later
+
+  const response = await fetch(
+    `https://slack.com/api/conversations.history?channel=${channelId}&limit=${limit}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (response.ok) {
+    const data = (await response.json()) as ISlackApiResponse;
+    if (data.ok) {
+      const texts = data.messages.map((message) => message.text);
+      return texts;
+    } else {
+      console.error('Slack API responded with an error:', data);
+    }
+  } else {
+    console.error('Failed to fetch Slack messages');
+  }
+}
