11547107de0d6e60a0b59946e7e82bf1a7e98171
Mads Baadsmand <baadsmand@gmail.com>
2023-12-06 17:05:40 +0100

    fix image paths

---
+++ Agenda-client/src/components/widget/widgets/Widgets.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+
+describe('Test widgets', ()=> {
+
+  describe('UserProfileWidget', ()=> {
+
+    it('should ')
+  });
+
+});
